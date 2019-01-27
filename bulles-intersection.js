0672936a221de041a676db63240bdaf38cf4735c
Benoit Brochier <benoit.brochier@gmail.com>
2019-01-27 17:33:18 +0100

    Update bulles-intersection.js

--- bulles-intersection.js
+++ bulles-intersection.js
@@ -2,31 +2,66 @@ var bulle = [];
 
 function setup() {
   createCanvas(600, 500);
+	bulleMouse = new BulleMouse();
 	for (var i = 0; i < 10; i++) {
-		bulle[i] = new Bulle(random(width), random(height), random(10,40));
+		bulle[i] = new Bulle(random(width), random(height), random(10,40), i);
 	}
 }
 
 function draw() {
   background(0);
 	for (var i = 0; i < bulle.length; i++) {
+		bulleMouse.intersects(bulle[i]);
 		for (var j = 0; j < bulle.length; j++) {
-			if (i !== j) {
+			if (i < j) {
 				bulle[i].intersects(bulle[j]);
 			}
 		}
 		bulle[i].move();
 	  bulle[i].display();
+		
+		bulleMouse.display();
 		stroke(255,255,255)
 		if (i < bulle.length - 1) {
-		  line(bulle[i].x, bulle[i].y, bulle[i+1].x, bulle[i + 1].y);
+		  //line(bulle[i].x, bulle[i].y, bulle[i+1].x, bulle[i + 1].y);
 		} else {
-		  line(bulle[i].x, bulle[i].y, bulle[0].x, bulle[0].y);
+		  //line(bulle[i].x, bulle[i].y, bulle[0].x, bulle[0].y);
 		}
 	}
 }
 
-function Bulle(x, y, r) {
+function BulleMouse() {
+  this.x = mouseX;
+	this.y = mouseY;
+	this.r = 30;
+	this.color = [255, 255, 255];
+	this.intersects = function(obj) {
+		var di = dist(mouseX, mouseY, obj.x, obj.y);
+		if(di < (this.r + obj.r)) {
+			if(mouseX < obj.x) {
+				obj.dX = 1;
+			}
+			if(mouseX > obj.x) {
+				obj.dX = -1;
+			}
+			if(mouseY < obj.y) {
+				obj.dY = 1;
+			}
+			if(mouseY > obj.y) {
+				obj.dY = -1;
+			}
+			this.color = obj.color;
+		}
+	}
+	this.display = function() {
+		fill(this.color[0], this.color[1], this.color[2]);
+		noStroke();
+		ellipse(mouseX, mouseY, (this.r * 2));
+	}
+}
+
+function Bulle(x, y, r, i) {
+	this.index = i;
 	this.x = x;
 	this.y = y;
 	this.r = r;
@@ -41,23 +76,25 @@ function Bulle(x, y, r) {
 	}
 	this.intersects = function(obj) {
 		var di = dist(this.x, this.y, obj.x, obj.y);
-		if (di < (this.r + obj.r)) {
-			if (this.x < obj.x) {
+		if(di < (this.r + obj.r)) {
+			if(this.x < obj.x) {
 				this.dX = -1;
 				obj.dX = 1;
 			}
-			if (this.x > obj.x) {
+			if(this.x > obj.x) {
 				this.dX = 1;
 				obj.dX = -1;
 			}
-			if (this.y < obj.y) {
+			if(this.y < obj.y) {
 				this.dY = -1;
 				obj.dY = 1;
 			}
-			if (this.y > obj.y) {
+			if(this.y > obj.y) {
 				this.dY = 1;
 				obj.dY = -1;
 			}
+			this.updateColor()
+			obj.updateColor()
 		}
 	}
 	this.move = function() {
@@ -91,8 +128,13 @@ function Bulle(x, y, r) {
 		}
 	}
 	this.display = function() {
-    noStroke()
-		fill(this.color[0], this.color[1], this.color[2]);
+		fill(0);
+		strokeWeight(2);
+		stroke(this.color[0], this.color[1], this.color[2]);
 		ellipse(this.x, this.y, (this.r * 2));
+		fill(255);
+		textSize(18);
+		textAlign(CENTER, CENTER);
+		text(this.index + 1, this.x , this.y)
 	}
 }
